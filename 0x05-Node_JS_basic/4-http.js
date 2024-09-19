510bfec28eec052b7ad816162444a81bf3eb471a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-09-19 22:41:29 +0300

    add 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(PORT, HOST);
+
+module.exports = app;
